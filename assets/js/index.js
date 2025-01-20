757d80580fa62dea6d452045057db59e3773dfc0
Truowng <quockhanhtruong166@gmail.com>
2025-01-21 00:12:23 +0700

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,46 +40,4 @@ if (submitBtn) {
       })
       .catch((error) => console.error("Error!", error.message));
   });
-
-  // form.addEventListener("submit", function (e) {
-  //   e.preventDefault(); // Prevent the default form submission
-
-  //   // Collect the form data
-  //   var formData = new FormData(this);
-  //   var keyValuePairs = [];
-  //   for (var pair of formData.entries()) {
-  //     keyValuePairs.push(pair[0] + "=" + pair[1]);
-  //   }
-
-  //   var formDataString = keyValuePairs.join("&");
-
-  //   // Send a POST request to your Google Apps Script
-  //   fetch("PATE YOURT URL WEBAPP ON GOOGLE SHEET", {
-  //     redirect: "follow",
-  //     method: "POST",
-  //     body: formDataString,
-  //     headers: {
-  //       "Content-Type": "text/plain;charset=utf-8",
-  //     },
-  //   })
-  //     .then(function (response) {
-  //       // Check if the request was successful
-  //       if (response) {
-  //         return response; // Assuming your script returns JSON response
-  //       } else {
-  //         throw new Error("Failed to submit the form.");
-  //       }
-  //     })
-  //     .then(function (data) {
-  //       document.getElementById("form").reset();
-  //     })
-  //     .catch(function (error) {
-  //       // Handle errors, you can display an error message here
-  //       console.error(error);
-  //     });
-  // });
-
-  // function getNewRecord() {
-  //   document.getElementById("form").reset();
-  // }
 }
